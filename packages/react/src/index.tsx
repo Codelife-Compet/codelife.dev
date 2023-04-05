4a43607beb46ebea58b96f3fa5c8851c470ff403
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2023-04-04 21:11:35 -0300

    Create a Button example

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import { ComponentProps } from 'react'
 import { styled } from '../styles'
-const Button = styled('button', {
-  fontFamily: '$default',
-  backgroundColor: '$default',
-  padding: '$4',
+export const Button = styled('button', {
+  fontFamily: '$body',
+  backgroundColor: '$lilac-dark',
+  color: '$gray100',
+  border: 0,
+  borderRadius: '$md',
+  fontWeight: '$bold',
+  variants: {
+    size: {
+      small: {
+        fontSize: '$sm',
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: '$xl',
+        padding: '$2 $4',
+      },
+    },
+  },
+  defaultVariants: {
+    size: 'small',
+  },
 })
-export default function app() {
-  return (
-    <>
-      <Button></Button>
-    </>
-  )
-}
+export type ButtonProps = ComponentProps<typeof Button>
