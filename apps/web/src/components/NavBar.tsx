18d000cb2a354774ac90b942f4b6f8b9f27a27e5
Francisco <franciscoabreu1408@gmail.com>
2023-05-11 16:09:15 -0300

    Adding the buttons

--- apps/web/src/components/NavBar.tsx
+++ apps/web/src/components/NavBar.tsx
@@ -1,35 +1,71 @@
 import Link from 'next/link'
 import { Button } from '@codelife-ui/react'
 import { useTranslation } from 'react-i18next'
+import { FaDoorOpen, FaUserPlus, FaQuestionCircle } from 'react-icons/fa'
+import Image from 'next/image'
+import SiteLogo from '../../public/codelife.svg'
 
 function NavBar() {
   const { t } = useTranslation()
 
   return (
-    <nav className="m-0 box-border flex items-center justify-between gap-8 px-4 py-0">
+    <nav className="m-0 box-border flex items-center justify-between gap-8 px-4 py-0 pr-25 pl-10 h-15">
       <Link href="#" className="">
-        CODELIFE
+        <Image src={SiteLogo} alt="Codelife" className="w-fit h-7 "></Image>
       </Link>
+      <div className="flex">
+        <Button
+          text
+          onClick={() => console.log('mock login with Google ')}
+          css={{
+            padding: '1px',
+            textTransform: 'uppercase',
+            fontSize: '$sm',
+            gap: '3px',
+            height: 'fit-content',
+          }}
+        >
+          <FaDoorOpen className="pb-0.5" />
+          {t('Enter')}
+        </Button>
 
-      <Button
-        text
-        onClick={() => console.log('mock login with Google ')}
-        css={{
-          '@phoneOnly': {
-            height: '$8',
-            fontSize: 'small',
-          },
-          backgroundColor: '#DB4437',
-          color: '$codelife-black-100',
-          borderColor: '#DB4437',
-          padding: '1px',
-          textTransform: 'uppercase',
-          fontSize: '$sm',
-          
-        }}
-      >
-        {t('Enter')}
-      </Button>
+        <Button
+          text
+          onClick={() => console.log('Create account')}
+          css={{
+            padding: '1px',
+            textTransform: 'uppercase',
+            fontSize: '$sm',
+            gap: '3px',
+            height: 'fit-content',
+          }}
+        >
+          <FaUserPlus className="pb-0.5" />
+          {t('Sign Up')}
+        </Button>
+
+        <Button
+          text
+          onClick={() => console.log('Create account')}
+          css={{
+            padding: '1px',
+            textTransform: 'uppercase',
+            fontSize: '$sm',
+            gap: '3px',
+            height: 'fit-content',
+          }}
+          className="hover:underline"
+        >
+          <FaQuestionCircle className="pb-0.5 h-100%" />
+          {t('About')}
+        </Button>
+
+        <div className="flex">
+          <Button text>Teste</Button>
+
+          <Button text>Teste2</Button>
+        </div>
+      </div>
     </nav>
   )
 }
