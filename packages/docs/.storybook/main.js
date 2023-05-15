1fb96e9e9c0f4049b8c4e7e9ee564f7deceb1ce3
Henrique de Paula Rodrigues <tec.henriquedepaula@gmail.com>
2023-05-15 15:29:04 -0300

    chore(docs): update dependencies

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,30 +1,24 @@
 import { mergeConfig } from 'vite';
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: [
-    "../src/pages/**/*.stories.mdx",
-    "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-a11y",
-    "@storybook/addon-actions",
-    "@storybook/addon-docs",
-  ],
+  stories: ["../src/pages/**/*.stories.mdx", "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-a11y", "@storybook/addon-actions", "@storybook/addon-docs", "@storybook/addon-mdx-gfm"],
   staticDirs: ['../public'],
-  docs:{
-    autodocs:'tag',
+  docs: {
+    autodocs: 'tag'
   },
   framework: {
     name: "@storybook/react-vite",
     options: {
-      builder:'@storybook/builder-vite'
-    },
+      builder: "@storybook/builder-vite"
+    }
   },
-  "features":{
-    storyStoreV7:true
-  },async viteFinal(config,{configType}) {
+  "features": {
+    storyStoreV7: true
+  },
+  async viteFinal(config, {
+    configType
+  }) {
     // Merge custom configuration into the default config
     if (configType === 'PRODUCTION') {
       // Your production configuration goes here.
@@ -33,11 +27,9 @@ const config = {
     return mergeConfig(config, {
       // Add dependencies to pre-optimization
       optimizeDeps: {
-        include: ['storybook-dark-mode'],
-      },
-      
-    }
-    );
-  },
+        include: ['storybook-dark-mode']
+      }
+    });
+  }
 };
 export default config;
