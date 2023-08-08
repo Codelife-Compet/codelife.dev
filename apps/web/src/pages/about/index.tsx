2c70c09786b8f20eb16991d67a0b3053f863ab65
Felipe Lacerda <felipelacerda500@gmail.com>
2023-08-08 11:32:22 -0300

    feat(codelife.dev/apps/web/src/pages/about/index.tsx): create About Codelife page
    This commit adds a new page "About" to the Codelife project. The "About" page provides information
    about the project's history and goals. The page layout was designed to match the project's overall
    theme and uses responsive design to ensure it looks great on various devices.
    
    "re #92"

--- apps/web/src/pages/about/index.tsx
+++ apps/web/src/pages/about/index.tsx
@@ -1,18 +1,47 @@
-import React, { useEffect, useState } from 'react'
-import { useTranslation } from 'react-i18next'
-
+import { Box, Heading, Text } from "@codelife-ui/react";
+import React, { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
 export default function About() {
-  const { t } = useTranslation()
-  const [hydrated, setHydrated] = useState(false)
+  const { t, i18n } = useTranslation();
+  const [hydrated, setHydrated] = useState(false);
+
   useEffect(() => {
-    setHydrated(true)
-  }, [])
+    setHydrated(true);
+  }, []);
+
+  const isPt = i18n.language.includes("pt");
+
   return (
     hydrated && (
-      <div>
-        <h1>{t('About')}</h1>
-        <p>{t('aboutP1')}</p>
-      </div>
+      <Box className="bg-white rounded-md mx-auto my-13 max-w-4xl p-13 w-full">
+        <Heading
+          size={"xl"}
+          style={{ lineHeight: "1.1" }}
+          className="font-heading mb-7 text-[#233233] font-bold"
+        >
+          {t("About")}
+        </Heading>
+        <Text className="mb-2 font-body text-codelife-black-500 font-normal text-md">
+          {t("aboutP1")}
+        </Text>
+        <Text className="mb-2 font-body text-codelife-black-500 font-normal text-md">
+          {t("aboutP2")}
+        </Text>
+        <Text className="mb-2 font-body text-codelife-black-500 font-normal text-md">
+          {t("aboutP3")}
+        </Text>
+        <Text className="mb-2 font-body text-codelife-black-500 font-normal text-md">
+          {t("aboutP4")}
+        </Text>
+        <Text className="mb-2 font-body text-codelife-black-500 font-normal text-md">
+          {t("aboutP5")}
+        </Text>
+        {isPt && (
+          <Text className="font-body text-codelife-black-500 font-normal text-md">
+            {t("aboutP6")}
+          </Text>
+        )}
+      </Box>
     )
-  )
+  );
 }
