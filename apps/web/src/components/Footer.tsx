7f20640aeb97ecb72d9880fe61c36ae8b55a017b
ooBraia <bryanalves4231@gmail.com>
2023-08-25 13:22:37 -0300

    fix: adicionando correção do redirecionamento das paginas de tradução
    96

--- apps/web/src/components/Footer.tsx
+++ apps/web/src/components/Footer.tsx
@@ -15,8 +15,17 @@ function Footer() {
   useEffect(() => {
     setHydrated(true)
   }, [])
-  const { t } = useTranslation()
+  const { t, 
+    i18n: {changeLanguage, language}, 
+    } = useTranslation()
 
+  const  [currentLanguage, setCurrentLanguage] = useState("language")
+
+  const handleChangeLanguage = () => {
+    const newLanguage = currentLanguage === 'en' ? 'pt' : 'en'
+    changeLanguage (newLanguage)
+    setCurrentLanguage(newLanguage)
+  }
   return hydrated ? (
     <footer className={styles.footer}>
       <div className={styles['inner-footer']}>
@@ -33,7 +42,7 @@ function Footer() {
                 <Link href="/about">{t('About')}</Link>
               </li>
               <li>
-                <Link href="/privacy">{t('Privacy Policy')}</Link>
+                <Link href="">{t('Privacy Policy')}</Link>
               </li>
               <li>
                 <Link href="">{t('Partners')}</Link>
@@ -87,13 +96,13 @@ function Footer() {
           <div className={styles['nav-footer-section']}>
             <h3>{t('Language')}</h3>
             <ul>
-              <li>
-                <Link href="/" locale="en">
+              <li onClick={handleChangeLanguage}>
+                <Link href="" locale="en">
                   {t('English')}
                 </Link>
               </li>
-              <li>
-                <Link href="/" locale="pt-BR">
+              <li onClick={handleChangeLanguage}>
+                <Link href="" locale="pt-BR">
                   {t('Portuguese')}
                 </Link>
               </li>
