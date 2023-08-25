3b8f16783c6f6d0ee405f1624c5c6ffa8fd9167f
PedroRonzani18 <pedroaugustogabironzani@hotmail.com>
2023-08-25 11:42:31 -0300

    style(backend): msotra se prisma conectou ao banco de dados
    msotra se prisma conectou ao banco de dados

--- apps/backend/src/core/env/index.ts
+++ apps/backend/src/core/env/index.ts
@@ -1,15 +1,14 @@
-import 'dotenv/config'; // carrega as variaveis ambiente
-import { z } from 'zod'; // contem as validações
+import 'dotenv/config'; 
+import { z } from 'zod'; 
 
 const envSchema = z.object({
 
-	NODE_ENV: z.enum(['dev', 'test', 'production']) // deve ser igual a uma dessas variaveis
-		.default('dev'), // caso nao informado, vira dev 
-
+	NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+	POSTGRESQL_USERNAME: z.string(),
+	POSTGRESQL_PASSWORD: z.string(),
+	POSTGRESQL_DATABASE: z.string(),
+	PORT: z.coerce.number().default(3333) 
 	// JWT_SECRET: z.string(),
-
-	PORT: z.coerce.number() // converte valor para um numero
-		.default(3333) // valor padrao: 3333
 });
 
 const _env = envSchema.safeParse(process.env); // tenta validar process.env para ver se tem as exatas informações dentro
@@ -18,7 +17,9 @@ if (_env.success === false) {
 	console.error('Invalid environment variables',
 		_env.error.format()); // formata todos os erros ali
 
-	throw new Error('Invalid environment variables'); // derruba aplicação
+	throw new Error('Invalid environment variables'); 
 }
 
-export const env = _env.data; // caso sucesso, exporta conteudo
+process.env.DATABASE_URL=`postgresql://${process.env.POSTGRESQL_USERNAME}:${process.env.POSTGRESQL_PASSWORD}@localhost:5432/${process.env.POSTGRESQL_DATABASE}?schema=public`
+
+export const env = _env.data;
