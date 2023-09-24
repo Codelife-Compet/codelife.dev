cf85183b4b76bc009d43ac204925cefacea8d81a
PedroRonzani18 <pedroaugustogabironzani@hotmail.com>
2023-09-24 19:31:14 -0300

    feat(backend): nova rota de retorno do perfil do usuario
    caso de uso, controller, factorie e alteração no repositorio para rota que retorna os dados do
    usuario pesquisando por seu id. Esta rota mostrou uma falha no id do usuario, que era recriado a
    cada chamada.   Problema resolvido

---
+++ apps/backend/src/domain/controllers/user/profile.ts
@@ -0,0 +1,35 @@
+import { makeCreateUserUseCase } from '@/domain/usecases/factories/make-create-user-use-case';
+import { makeGetUserProfileUseCase } from '@/domain/usecases/factories/make-get-user-profile-use-case';
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { z } from 'zod';
+
+export const createUserBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+	token: z.string(),
+	token_type: z.enum(["google", "facebook", "github"])
+});
+
+export async function profile(request: FastifyRequest, reply: FastifyReply) {
+
+	const getUserProfile = makeGetUserProfileUseCase();
+
+	const user = await getUserProfile.execute({
+		userId: request.user.sub
+	});
+    if (user.isLeft()) {
+        return reply
+            .status(400)
+            .send({ error_message: user.value.message })
+    }
+
+
+	return reply
+		.status(200)
+		.send({
+			user: {
+				...user.value.user,
+				password_hash: undefined // para segurança 
+			}
+		}); 
+}
