eb35b2e34c1a050161ab38fc54c076a38267cf88
PedroRonzani18 <pedroaugustogabironzani@hotmail.com>
2023-09-24 19:26:58 -0300

    chore(backend): implementação dos modulos de token no app
    implementação dos modulos de cooke (refreshToken) e tokens de autenticação

--- apps/backend/src/core/app.ts
+++ apps/backend/src/core/app.ts
@@ -1,6 +1,22 @@
 import { userRoutes } from '@/domain/controllers/user/routes'
+import fastifyJwt from '@fastify/jwt'
 import fastify from 'fastify'
+import { env } from './env';
+import fastifyCookie from '@fastify/cookie';
 
 export const app = fastify()
+
+app.register(fastifyJwt, {
+	secret: env.JWT_SECRET,
+	cookie: {
+		cookieName: 'refreshToken',
+		signed: false // esse cookie n é assinado (hashed)
+	},
+	sign: {
+		expiresIn: '10m' // token padrão será revalidado a cada 10 min pelo refresh
+	}
+});
+
+app.register(fastifyCookie)
 
 app.register(userRoutes, { prefix: 'user' })
