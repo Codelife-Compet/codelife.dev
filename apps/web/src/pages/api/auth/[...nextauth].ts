58ae4fc5d54ac7a57279334b3d489a9b59e75d35
Francisco <franciscoabreu1408@gmail.com>
2023-11-05 12:51:04 -0300

    feat: excluindo o adapter do Prisma

--- apps/web/src/pages/api/auth/[...nextauth].ts
+++ apps/web/src/pages/api/auth/[...nextauth].ts
@@ -1,14 +1,14 @@
-import { PrismaAdapter } from '@auth/prisma-adapter'
+//import { PrismaAdapter } from '@auth/prisma-adapter'
 import NextAuth from 'next-auth'
 
-import { PrismaClient } from '@prisma/client'
+//import { PrismaClient } from '@prisma/client'
 import GithubProvider from 'next-auth/providers/github'
 import GoogleProvider from 'next-auth/providers/google'
 
-const prisma = new PrismaClient()
+//const prisma = new PrismaClient()
 
 export default NextAuth({
-  adapter: PrismaAdapter(prisma),
+  //adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: 'aa7cf54cce4fd4eb1d32',
