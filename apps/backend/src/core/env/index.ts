2628f8fa29e0a35d757871b03e6f9f7e13822629
PedroRonzani18 <pedroaugustogabironzani@hotmail.com>
2023-11-06 11:05:34 -0300

    feat(backend): env com dados de AWS
    env com dados de AWS

--- apps/backend/src/core/env/index.ts
+++ apps/backend/src/core/env/index.ts
@@ -10,6 +10,9 @@ const envSchema = z.object({
 	DATABASE_URL: z.string().optional(),
 	PORT: z.coerce.number().default(3333), 
 	JWT_SECRET: z.string(),
+	AWS_ACCESS_KEY_ID: z.string(),
+	AWS_SECRET_ACCESS_KEY: z.string(),
+	AWS_BUCKET_NAME: z.string(),
 });
 
 const _env = envSchema.safeParse(process.env); // tenta validar process.env para ver se tem as exatas informações dentro
